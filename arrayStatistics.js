b5bdec92186a2ed875c5936e013dc6c4abbaf279
kelia01 <iradukundakelia@gmail.com>
2024-11-05 12:50:09 +0200

    Fix sum and average Add sum  function

--- arrayStatistics.js
+++ arrayStatistics.js
@@ -1,8 +1,8 @@
-/*You are given an array of numbers. Your task is to implement a function called arrayStats that returns an object containing various statistics about the array.
+/*You are given an array of . Your task is to implement a function called arrayStats that returns an object containing various statistics about the array.
 
 The returned object should have the following properties:
-sum: The sum of all numbers in the array.
-average: The average of all numbers in the array (rounded to two decimal places).
+sum: The sum of all  in the array.
+average: The average of all  in the array (rounded to two decimal places).
 min: The minimum value in the array.
 max: The maximum value in the array.
 
@@ -16,22 +16,27 @@ arrayStats([1, 2, 3, 4, 5]); // should return { sum: 15, average: 3.00, min: 1, 
 */
 var arrayStats = function(numbers){
 var numberObj = {
-  sum: 0;
-  add: function(numbers){
+  sum: function(){
+    var sum=0;
     for(var i = 0; i<numbers.length; i++){
-      numberObj.sum+= numbers[i];
+       sum+= numbers[i];
     } 
+    return sum;
     },
-  average: function(numbers){
-  var average = numberObj.sum/(numbers.length);
+  average: function(){
+  var average = 0;
+  var total = this.sum();
+  average= (this.sum()/numbers.length.toFixed(2));
+  return average;
   },
-   min: function(numbers){
+   min: function(){
      var min=numbers[0];
      for(var i=0;i<numbers.length;i++){
      if(numbers[i]<min){
      min=numbers[i];
      }
      }
+     return min;
      },
   max: function(){
     var max=numbers[0];
@@ -40,6 +45,14 @@ var numberObj = {
      max=numbers[i];
     }
     }
+    return max
   }
-  }
+};
+return {
+  sum: numberObj.sum(),
+  average: parseFloat(numberObj.average()),
+  min: numberObj.min(),
+  max: numberObj.max()
+}
+
 }
